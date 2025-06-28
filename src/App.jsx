f6696949dcf2b4af1f78f7b7093f88799f3cd86a
Suraj-Nagar <surajnagar5755@gmail.com>
2025-06-28 14:04:01 +0530

    added footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-function App() {
- 
+import './App.css';
+import { Routes } from 'react-router-dom';
+import Footer from './Components/Footer';
 
+function App() {
   return (
     <>
-    <h1 className="font-bold text-3xl underline text-stone-50">jay shree ram</h1>
+      <h1 className="font-bold text-3xl underline text-stone-50">jay shree ram</h1>
+      <Footer/>
     </>
   )
 }
