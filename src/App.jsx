08c6016bbedb2268a6f14c3cb07b8b3e885e45fe
Suraj-Nagar <surajnagar5755@gmail.com>
2025-07-06 23:57:23 +0530

    add notfound

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import { Route, Routes } from 'react-router-dom';
 
 import HomePage from './Pages/HomePage';
 import AboutUs from './Pages/AboutUs';
+import NotFound from './Pages/NotFound';
 
 function App() {
-  return (
+  return (  
     <>
 
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutUs />} />
+        <Route path='*' element={<NotFound/>}></Route>
       </Routes> 
     </>
   )
