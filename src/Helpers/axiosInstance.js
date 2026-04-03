515bc219377f3c9541a68c2c4b48d6e472205028
Suraj-Nagar <surajnagar5755@gmail.com>
2026-04-03 16:36:00 +0530

    update

--- src/Helpers/axiosInstance.js
+++ src/Helpers/axiosInstance.js
@@ -4,7 +4,7 @@ const BASE_URL="http://localhost:5015/api/v1";
 
 const axiosInstance=axios.create();
 
-axiosInstance.defaults.baseURL=import.meta.env.REACT_APP_BASE_URL||BASE_URL;
+axiosInstance.defaults.baseURL=import.meta.env.VITE_API_URL||BASE_URL;
 axiosInstance.defaults.withCredentials=true;
  
 export default axiosInstance;
