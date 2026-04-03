20c217fc883e3fe333216b1a20ff97c29cb5753b
Suraj-Nagar <surajnagar5755@gmail.com>
2026-04-03 16:11:40 +0530

    update  file with the backend URL

--- src/Helpers/axiosInstance.js
+++ src/Helpers/axiosInstance.js
@@ -1,12 +1,10 @@
 import axios from "axios";
-import dotenv from "dotenv";
 
-dotenv.config();
 const BASE_URL="http://localhost:5015/api/v1";
 
 const axiosInstance=axios.create();
 
-axiosInstance.defaults.baseURL=process.env.REACT_APP_BASE_URL||BASE_URL;
+axiosInstance.defaults.baseURL=import.meta.env.REACT_APP_BASE_URL||BASE_URL;
 axiosInstance.defaults.withCredentials=true;
  
 export default axiosInstance;
